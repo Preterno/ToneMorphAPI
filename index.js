c50d34e88f2ffd51e7b8530e2f92a6e8132387fd
Preterno <mohammedaslam8483@gmail.com>
2025-01-19 15:38:06 +0530

    Update cors policy

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ const corsOptions = {
   allowedHeaders: "Content-Type,Authorization",
 };
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
 app.use(express.static("public"));
